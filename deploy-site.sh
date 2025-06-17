0450eed6e2a8dbb406f7d2c7a0bae1f5fd790d2c
opeyemi <opeyemibeloved00@gmail.com>
2025-06-17 19:10:46 +0100

    Fix line endings in deploy script

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Automate Git commit and push for GitHub Pages
 git add .
-git commit -m "Update static website files" || echo "No changes to commit"
-git push origin master || echo "Push failed
+git commit -m "Update static_website files" || echo "No changes to commit"
+git push origin master || echo "Push failed"
+
+
+
